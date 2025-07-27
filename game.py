c0bc6fca93042868104d914951c4cb696b5c76a6
Paulin <paulohspfc209@gmail.com>
2025-07-26 23:12:22 -0300

    Update

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ import sys
 
 display=pygame.display.set_mode((1280, 720))
 
-player1 = pygame.Rect(100, 100, 50, 50)
+player1 = pygame.Rect(0, 0, 50, 50)
 player2 = pygame.Rect(200, 100, 50, 50)
 ball= pygame.Rect(600,123, 323, 223)
 
@@ -14,6 +14,15 @@ while loop:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             loop = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player1.x += 50
+            elif event.key == pygame.K_a:
+                player2.y -= 50
+            elif event.key == pygame.K_d:
+                ball.x += 50
+
 
     pygame.display.flip()
 
